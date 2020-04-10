b26171ffd98f72999ac228eddf33ccaafba54cda
bjs20 <bjs20@175.197.54.89>
2020-04-10 19:31:36 +0900

    Init Calender project

---
+++ src/jiseock/calender/Calender.java
@@ -0,0 +1,10 @@
+package jiseock.calender;
+
+public class Calender {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Calender");
+	}
+
+}
