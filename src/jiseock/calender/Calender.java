8e925d506c3e80cf846ecad0cafc5e228d78e25c
bjs20 <bjs20@221.150.101.208>
2020-04-28 20:19:56 +0900

    Update using class method

--- src/jiseock/calender/Calender.java
+++ src/jiseock/calender/Calender.java
@@ -3,24 +3,32 @@ package jiseock.calender;
 import java.util.Scanner;
 
 public class Calender {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month -1];
+	}
+	public void printSampleCalender() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("-------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		System.out.printf("%d월은 %d일까지 있습니다. \n",month,maxDays[month-1]);
+		System.out.printf("%d월은 %d일까지 있습니다. \n",month,cal.getMaxDaysOfMonth(month-1));	
+		
+		cal.printSampleCalender();
+		
 		scanner.close();
 	}
 	
